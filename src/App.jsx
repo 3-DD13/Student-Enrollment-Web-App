8c764ce9bbe297740e155123c8acd64d822481ff
Diego Silva <diegosilva@unknown4e630f9a3a64.attlocal.net>
2026-06-18 13:48:44 -0700

    added a add courses thing

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,56 @@ function App() {
   const [password, setPassword] = useState("");
   const [username, setUsername] = useState("");
 
+  const[page, setPage] = useState("Courses");
+  const[myCourses, setMyCourses] = useState([
+    {
+      name: "physics009" ,
+      Teacher: "John",
+      time: "MON 11:00-12:15 AM",
+      enrolled: 6 , 
+      capacity: 10
+
+    },
+    {
+      name: "CSE 106" ,
+      Teacher: "Ammon Hepworth",
+      time: "WF 10:00-12:20 AM",
+      enrolled: 8 , 
+      capacity: 10
+    }
+
+  ]);
+
+    const [allCourses, setAllCourses] = useState([
+      {
+        name: "Math 141",
+        Teacher: "Heinrich Müller",
+        time: "TTh 1:00-2:15 PM",
+        enrolled: 10,
+        capacity: 10
+      }, 
+      {
+        name: "CSE 900",
+        Teacher: "Ammon Hepworth",
+        time: "MWF 3:00-4:00 PM",
+        enrolled: 8,
+        capacity: 10
+      }
+    ]);
+
+
   if (count) {
     return (
       <>
         <h1>ACME University</h1>
         <h2>Welcome {username}</h2>
+        <button onClick={() => setPage("Courses")}> Courses</button>
+        <button onClick={() => setPage("addCourses")}> Add Courses</button>
+
+
         <button onClick={() =>setCount(false)}> Sign out</button>
 
-        <h2> courses</h2>
+        <h2>{page === "Courses" ? "Your Courses" : "All Courses"}</h2>
 
         <table>
           <thead>
@@ -23,21 +65,17 @@ function App() {
               <th>Student enrolled</th>
             </tr>
           </thead>
-        <tbody> 
-          <tr> 
-            <td> Physics 009</td>
-            <td> john</td>
-            <td> MON 11:00-12:15 AM </td>
-            <td> 6/10 </td>
-          </tr>
-          <tr>
-            <td> CSE 106 </td> 
-            <td>Ammon Hepworth</td> 
-            <td> MWF 10:00-12:20 AM</td> 
-            <td> 8/10</td> 
-          </tr>
-        </tbody>
-        </table>
+        <tbody>
+  {(page === "Courses" ? myCourses : allCourses).map((course) => (
+    <tr key={course.name}>
+      <td>{course.name}</td>
+      <td>{course.teacher}</td>
+      <td>{course.time}</td>
+      <td>{course.enrolled}/{course.capacity}</td>
+    </tr>
+  ))}
+       </tbody>
+      </table>
       </>
     );
   }
@@ -68,12 +106,7 @@ return (
     </label>
   </form>
 
-  <input 
-  type="text"
-  value={username}
-  onChange={(e) => setUsername(e.target.value)}
-/>
-
+  
 <button type="button" className="signIn" onClick={() => setCount(true)}>
   Sign in
 </button>
